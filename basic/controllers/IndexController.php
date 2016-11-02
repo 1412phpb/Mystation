9b5632323f7a19ab2f788597be95caabb02ef950
likang0715 <308038516@qq.com>
2016-11-02 10:27:01 +0800

    Create IndexController.php

---
+++ basic/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+use app\models\Users;
+use yii\helpers\Url;
+use yii\data\Pagination;
+
+class UsersController extends Controller
+{
+
+}
+?>
